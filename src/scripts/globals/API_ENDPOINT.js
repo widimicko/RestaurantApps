0fee57ee69df9e6be2c4e8b037e63c407fa5bf84
Micko Widi <widimicko85@gmail.com>
2022-01-17 10:50:55 +0700

    fix api endpoint

--- src/scripts/globals/API_ENDPOINT.js
+++ src/scripts/globals/API_ENDPOINT.js
@@ -1,7 +1,7 @@
 import CONFIG from './CONFIG'
 
 const API_ENDPOINT = {
-  restaurant: `${CONFIG.BASE_URL}list/`,
+  restaurant: `${CONFIG.BASE_URL}list`,
   detail: (id) => `${CONFIG.BASE_URL}detail/${id}`,
   addReview: `${CONFIG.BASE_URL}review`,
   search: (query) => `${CONFIG.BASE_URL}search?q=${query}`
